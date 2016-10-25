7aa31974b6a4828fdd8d1520a72748fe8828239b
Patrick Pokatilo <mail@shyxormz.net>
2016-10-25 12:34:52 +0200

    + FindApache2 (POC)

---
+++ FindApache2.cmake
@@ -0,0 +1,18 @@
+include(FindPackageHandleStandardArgs)
+
+find_path(Apache2_INCLUDE_DIR
+        NAMES httpd.h
+        HINTS ENV APACHE2DIR
+        PATHS /usr/local/apache2
+        PATH_SUFFIXES include)
+
+find_library(Apache2_LIBRARY
+        NAMES apr-2
+        HINTS ENV APACHE2DIR
+        PATHS /usr/local/apache2
+        PATH_SUFFIXES lib)
+
+set(Apache2_LIBRARIES ${Apache2_LIBRARY} CACHE STRING "")
+
+find_package_handle_standard_args(Lua
+        REQUIRED_VARS Apache2_LIBRARIES Apache2_INCLUDE_DIR)
