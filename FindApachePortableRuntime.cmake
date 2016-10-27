967ccdd45890dc7c3028f1504be725c6dc433946
Patrick Pokatilo <mail@shyxormz.net>
2016-10-27 17:04:01 +0200

    Add more potential paths for APR

--- FindApachePortableRuntime.cmake
+++ FindApachePortableRuntime.cmake
@@ -4,7 +4,7 @@ find_path(ApachePortableRuntime_INCLUDE_DIR
         NAMES apr.h
         HINTS ENV APACHEPORTABLERUNTIMEDIR
         PATHS /usr/local/apache2
-        PATH_SUFFIXES include include/apr-2 include/apr-1)
+        PATH_SUFFIXES include include/apr-2 include/apr-2.0 include/apr-1 include/apr-1.0)
 
 find_library(ApachePortableRuntime_LIBRARY
         NAMES apr-2 apr-1
