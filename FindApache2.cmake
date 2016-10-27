c8a4be264a4bd2f0015ab2fa7081f430e7b1687f
Patrick Pokatilo <mail@shyxormz.net>
2016-10-27 16:46:05 +0200

    Add more path suffixes, reorder paths

--- FindApache2.cmake
+++ FindApache2.cmake
@@ -4,7 +4,7 @@ find_path(Apache2_INCLUDE_DIR
         NAMES httpd.h
         HINTS ENV APACHE2DIR
         PATHS /usr/local/apache2
-        PATH_SUFFIXES include)
+        PATH_SUFFIXES include include/apache24 include/apache22)
 
 find_package_handle_standard_args(Apache2
         REQUIRED_VARS Apache2_INCLUDE_DIR)
--- FindApachePortableRuntime.cmake
+++ FindApachePortableRuntime.cmake
@@ -4,7 +4,7 @@ find_path(ApachePortableRuntime_INCLUDE_DIR
         NAMES apr.h
         HINTS ENV APACHEPORTABLERUNTIMEDIR
         PATHS /usr/local/apache2
-        PATH_SUFFIXES include include/apache22 include/apache24)
+        PATH_SUFFIXES include include/apr-2 include/apr-1)
 
 find_library(ApachePortableRuntime_LIBRARY
         NAMES apr-2 apr-1
